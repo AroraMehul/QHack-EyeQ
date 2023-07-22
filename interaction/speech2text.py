5211743b910ac43ac021434bf2854a6cc8bdc341
VrutikShah <vrshah@ucsd.edu>
2023-07-22 16:36:59 -0700

    small change

--- interaction/speech2text.py
+++ interaction/speech2text.py
@@ -3,3 +3,11 @@ Script to record audio in English and convert to text
 """
 
 import speech_recognition as sr
+
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = sr.record(source, duration=5)
+    print("Recognizing...")
+    # convert speech to text
+    text = sr.recognize_google(audio_data)
+    print(text)
