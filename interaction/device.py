0faa66327dfa8405f5b4db23b2ee783bc5477ceb
VrutikShah <vrshah@ucsd.edu>
2023-07-23 19:11:15 -0700

    added dataset

--- interaction/device.py
+++ interaction/device.py
@@ -6,7 +6,9 @@ import mac_say
 
 
 def speak(audio):
-    mac_say.say(audio)
+    mac_say.say([audio,'-v','Samantha'])
+    # mac_say.gtts.say("en",audio)
+    # print(mac_say.voices("en"))
     # engine.say(audio)
     # engine.runAndWait()
 
@@ -17,7 +19,7 @@ def takeCommand():
     with m as source:
         r.adjust_for_ambient_noise(source)
         print("Listening...")
-        r.pause_threshold = 1
+        # r.pause_threshold = 1
         audio = r.listen(source, phrase_time_limit=3)
 
     try:
@@ -48,15 +50,23 @@ if __name__ == '__main__':
     while True:
 
         query = takeCommand().lower()
+        if "IQ" in query or 'queue' in query or 'hello' in query:
+            speak("How may i help you")
 
-        if "remember" in query:
-            speak("name this object")
+        elif 'navigation' in query or 'start' in query:
+            speak("Starting camera and mapping")
+
+        elif "remember" in query or 'object' in query:
+            speak("please name this object")
             obj_name = takeCommand()
             speak(f'{obj_name} is saved at this location')
 
         elif 'where is' in query:
             query = query.replace('where is', '')
             speak(f"Looking for {query}")
+            # table
+            time.sleep(1)
+            speak(f"Found {query} about one meters straight and slight right from your current location")
 
         elif "read" in query:
             speak("reading")
@@ -65,14 +75,37 @@ if __name__ == '__main__':
             speak(text)
         elif "look for" in query:
             # runImagenetmodel
+            # chair
             query = query.replace('look for', '')
             speak(f'Looking for {query}')
+            time.sleep(2)
+            speak(f"found {query}, one point five meters straight and slight left from your current location")
+
         elif 'surrounding' in query:
             # run img captioning model
             speak('In front of you, there is a pervert sitting with Mehul Arora')
         # elif "camera" in query or "take a photo" in query:
         # 	ec.capture(0, "Jarvis Camera ", "img.jpg")
+
+        elif 'take me' in query:
+            query = query.replace('take me to','')
+            speak(f'Fetching location of {query}')
+            time.sleep(1)
+            speak(f'Generating path to {query}. Please wait')
+            time.sleep(3)
+            speak(f'Starting navigation to {query}. Go Straight for 3.5 meters.')
+            time.sleep(1.5)
+            speak('go straight')
+            time.sleep(1)
+            speak('turn left')
+            time.sleep(2)
+            speak('go straight')
+            time.sleep(2)
+            speak(f'you have arrived. {query} is on the left')
             
+
+
+
         elif "what is" in query or "who is" in query:
             
             # Use the same API key
---
+++ tasks.md
@@ -0,0 +1,29 @@
+# demo clip 1
+1. look for a table around me
+2. register this object
+
+# demo clip 2
+1. take me to object
+2. navigation
+
+# demo clip 3
+1. read what's on the page
+
+# demo clip 4
+1. describe the surrounding?
+
+# demo clip 5
+1. google navigation?
+
+
+# ppt
+
+1. intro
+2. motivation
+3. concepts
+4. assumptions
+5. features
+6. demo
+7. feasibility study
+8. limitations and future work
+9. 
